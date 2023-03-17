8f7a96067ada2754ebb146b3959cb1051af9dfe2
박준형 <108733795+krapli441@users.noreply.github.com>
2023-03-17 16:39:23 +0900

    콜백함수..연습

--- callback.js
+++ callback.js
@@ -10,19 +10,30 @@ function CreaDoc(Tag, Parents, Attri, Attrivalue) {
 
 // CreaDoc('div', root, 'class', 'title');
 
+const arrays = [];
+console.log(arrays);
+
 function timer() {
   setTimeout(function(){
     CreaDoc('div', root, 'class', 'title');
+    arrays.push('first');
+    console.log(arrays);
     console.log("첫번째 함수");
     setTimeout(function(){
       console.log("두번째 함수");
       CreaDoc('div', root, 'class', 'title');
+      arrays.push('second');
+      console.log(arrays);
       setTimeout(function(){
         console.log("세번째 함수");
         CreaDoc('div', root, 'class', 'title');
+        arrays.push('third');
+        console.log(arrays);
         setTimeout(function(){
           console.log("네번째 함수")
           CreaDoc('div', root, 'class', 'title');
+          arrays.push('fourth');
+          console.log(arrays);
         },1000);
       },1000);
     },1000);  
