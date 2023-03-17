9803697904bc8de576dcf11d0cd4ef42ed116669
박준형 <108733795+krapli441@users.noreply.github.com>
2023-03-17 14:58:24 +0900

    타이머 콜백함수

---
+++ callback.js
@@ -0,0 +1,16 @@
+
+
+function timer() {
+  setTimeout(function(){
+    console.log("첫번째 함수");
+    setTimeout(function(){
+      console.log("두번째 함수");
+      setTimeout(function(){
+        console.log("세번째 함수");
+        setTimeout(function(){
+          console.log("네번째 함수")
+        },1000);
+      },1000);
+    },1000);  
+  },1000);
+}
