3ad6f500ac35a0fbf8529c6bba58500567247b96
박준형 <108733795+krapli441@users.noreply.github.com>
2023-03-17 15:39:29 +0900

    첫번째 예제

--- callback.js
+++ callback.js
@@ -1,14 +1,28 @@
+const root = document.getElementById('root');
 
+function CreaDoc(Tag, Parents, Attri, Attrivalue) {
+  const element = document.createElement(Tag);
+  Parents.appendChild(element);
+  if (Attri !== undefined) {
+    element.setAttribute(Attri, Attrivalue);
+  }
+}
+
+// CreaDoc('div', root, 'class', 'title');
 
 function timer() {
   setTimeout(function(){
+    CreaDoc('div', root, 'class', 'title');
     console.log("첫번째 함수");
     setTimeout(function(){
       console.log("두번째 함수");
+      CreaDoc('div', root, 'class', 'title');
       setTimeout(function(){
         console.log("세번째 함수");
+        CreaDoc('div', root, 'class', 'title');
         setTimeout(function(){
           console.log("네번째 함수")
+          CreaDoc('div', root, 'class', 'title');
         },1000);
       },1000);
     },1000);  
